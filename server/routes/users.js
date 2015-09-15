5be2a516b663ae8d08c9151c72d0f49440be1274
Blackstock <charlieblackstock@gmail.com>
2015-09-15 15:08:51 -0600

    refactor routes into promises

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,12 +6,38 @@ var User = require('../models/user.js');
 
 router.get('/users', function(req, res, next) {
   User.findQ()
-    .then(function (result) { res.json(result) })
-    .catch(function (err) {res.send(err) })
+    .then(function (result) { res.json(result);})
+    .catch(function (err) {res.send(err);})
     .done();
 });
 
 //// Write the rest of this Crud app using promises instead of callbacks.
 
+router.post('/users', function(req, res, next) {
+  var newUser = new User({username: req.body.name});
+  newUser.saveQ()
+    .then(function (result) {res.json(result);})
+    .catch(function (err) {res.send(err);})
+    .done();
+});
+
+router.get('/user/:id', function(req, res, next) {
+  User.findById(req.params.id)
+    .then(function(result) {res.json(result);}) .catch(function(err) {res.send(err);})
+    .done();
+});
+
+router.put('/user/:id', function(req, res, next) {
+  var update = {username: req.body.name};
+  User.findByIdAndUpdate(req.params.id, update)
+    .then(function(result) {res.json(result);}) .catch(function(err) {res.send(err);})
+    .done();
+});
+
+router.delete('/user/:id', function(req, res, next) {
+  User.findByIdAndRemove(req.params.id)
+    .then(function(result) {res.json(result);}) .catch(function(err) {res.send(err);})
+    .done();
+});
 
 module.exports = router;
